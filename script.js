507a20a6b47bce8e7e6d5928617e6a27d529d9c2
Justin Bento <44421842+Justin-Bento@users.noreply.github.com>
2024-12-20 15:26:44 -0400

    Changed this to objects

--- script.js
+++ script.js
@@ -1,15 +1,18 @@
-// Store the gameboard as an array inside Gameboard object.
 const gameDisplay = document.querySelector(".gameboard");
 
-function ticTacToeBoard() {
-  let gameboard = ["0", "1", "2", "3", "4", "5", "6", "7", "8"];
-  gameboard.forEach((game, i) => {
-    const paragraph = document.createElement("p");
-    paragraph.textContent = game;
-    paragraph.addEventListener("click", () => {
-      console.log(game++);
+const ticTacToe = {
+  gameboard: ["1", "1", "2", "3", "4", "5", "6", "7", "8"],
+  displayBoard: function () {
+    this.gameboard.forEach((game, i) => {
+      const paragraph = document.createElement("p");
+      paragraph.textContent = game;
+      paragraph.addEventListener("click", () => {
+        console.log(game++);
+      });
+      gameDisplay.appendChild(paragraph);
     });
-    gameDisplay.appendChild(paragraph);
-  });
-}
-ticTacToeBoard();
+  },
+  player: function () {},
+};
+
+ticTacToe.displayBoard();
