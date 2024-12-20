6c44fde2aeff23646c1b434d753c2b455e94a8aa
Justin Bento <44421842+Justin-Bento@users.noreply.github.com>
2024-12-20 11:41:38 -0400

    Created a visual representation of the tic tac toe board inside the javascript console.

---
+++ script.js
@@ -0,0 +1,7 @@
+// Store the gameboard as an array inside Gameboard object.
+const gameDisplay = document.querySelector(".gameboard");
+
+function ticTacToeBoard() {
+  return "|───|───|───|\n|───|───|───|\n|───|───|───|";
+}
+console.log(ticTacToeBoard());
