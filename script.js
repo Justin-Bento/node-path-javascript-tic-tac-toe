f028c33d9d15df6511ee453aef805852d6e53259
Justin Bento <44421842+Justin-Bento@users.noreply.github.com>
2024-12-20 12:50:50 -0400

    Created the logic for the boarders

--- script.js
+++ script.js
@@ -2,9 +2,11 @@
 const gameDisplay = document.querySelector(".gameboard");
 
 function ticTacToeBoard() {
-  let gameboard = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-  gameboard.forEach((box) => {
-    console.log(box);
+  let gameboard = ["0", "1", "2", "3", "4", "5", "6", "7", "8"];
+  gameboard.forEach((game, i) => {
+    const paragraph = document.createElement("p");
+    paragraph.textContent = game;
+    gameDisplay.appendChild(paragraph);
   });
 }
 ticTacToeBoard();
