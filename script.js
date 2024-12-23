05b12a76a0e190182b7809db7a26a55ea1cfd4dc
Justin Bento <44421842+Justin-Bento@users.noreply.github.com>
2024-12-23 11:40:43 -0400

    changed variable and element to div

--- script.js
+++ script.js
@@ -4,16 +4,16 @@ const gameboard = ["1", "1", "2", "3", "4", "5", "6", "7", "8"];
 const ticTacToe = {
   displayBoard: function () {
     gameboard.forEach((game, i) => {
-      const paragraph = document.createElement("p");
-      paragraph.textContent = game;
-      paragraph.setAttribute("class", "hide");
-      paragraph.style.color = "rgba(0, 0, 0, 0)"; // Hide initially
-      paragraph.addEventListener("click", () => {
-        paragraph.textContent = "X";
-        paragraph.classList.add("showText");
-        paragraph.style.color = "rgba(0, 0, 0, 1)"; // Show text
+      const container = document.createElement("div");
+      container.textContent = game;
+      container.setAttribute("class", "hide");
+      container.style.color = "rgba(0, 0, 0, 0)"; // Hide initially
+      container.addEventListener("click", () => {
+        container.textContent = "X";
+        container.classList.add("showText");
+        container.style.color = "rgba(0, 0, 0, 1)"; // Show text
       });
-      gameDisplay.appendChild(paragraph);
+      gameDisplay.appendChild(container);
     });
   },
 };
