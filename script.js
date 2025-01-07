55d3470792f1a57e59c7005918fa98726f74246a
Justin Bento <44421842+Justin-Bento@users.noreply.github.com>
2025-01-07 10:26:54 -0400

    reset the javascript boxes.

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
 const gameDisplay = document.querySelector(".gameboard");
-const gameboard = ["1", "1", "2", "3", "4", "5", "6", "7", "8"];
+const gameboard = ["0", "1", "2", "3", "4", "5", "6", "7", "8"];
 const startGame = document.querySelector(".start-game");
+
+const ticTacToe = {
+  boxes: gameboard.map((x) => {
+    const box = document.createElement("div");
+    box.textContent = x;
+    gameDisplay.appendChild(box); // Append each box to gameDisplay
+    return box; // Return the box element for future reference
+  }),
+};
